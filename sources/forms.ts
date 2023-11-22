fbdb22d63bceabe06e5295947a1db3f8225426b8
Berend Vandenbussche <berend@inuits.eu>
2023-11-22 14:29:37 +0100

    Remove old form

--- sources/forms.ts
+++ sources/forms.ts
@@ -1,17 +1,3 @@
-import {
-  Form,
-} from '../../../generated-types/type-defs';
+import { Form } from "../../../generated-types/type-defs";
 
-
-export const AssetForm: Form = {
-  fields: [
-    {
-      key: 'relatie',
-      label: 'relatie',
-      relationType: 'components',
-      metadata: [],
-      acceptedEntityTypes: ['asset'],
-      disabled: false,
-    },
-  ],
-};
+export const AssetForm: Form = {};
